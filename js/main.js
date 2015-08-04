53a337774a21fb16d366a250d89d200f54d121f7
Sudo <key_mtrgtring@yahoo.co.jp>
2015-08-04 22:46:27 +0900

    課題JS-1(最低限の実装)を追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+function parseLTSVLog(logStr){
+	if (logStr == ""){
+		var parseStr = [];
+	} else {
+		var parseStr = [];
+		var curStr = logStr.split("\n");
+		for(var i=0;i<curStr.length;i++){
+			if (curStr[i] == ""){
+				continue
+			} else {
+				console.log("curstr:"+curStr[i]);
+				var parseLineStr = {};
+				var epochIndex = curStr[i].indexOf("epoch:");
+				var pathIndex = curStr[i].indexOf("path:");
+				var epochValue = curStr[i].slice(epochIndex+"epoch:".length);
+				var pathValue = curStr[i].slice(pathIndex+"path:".length,epochIndex).replace(/^\s+|\s+$/g, "");
+				parseLineStr.path = pathValue;
+				parseLineStr.epoch = Number(epochValue);
+				parseStr.push(parseLineStr);
+			}
+		}
+	}
+
+	return parseStr
+}
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
