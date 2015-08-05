5df7a8a107faa0b60b405dec239679ce6b3a614e
Sudo <key_mtrgtring@yahoo.co.jp>
2015-08-05 13:24:35 +0900

    課題JS-3(最低限の実装)を追加

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,7 @@
 // 課題 JS-3 の実装をここに記述してください。
+window.onload = function() {
+	document.getElementById("submit-button").onclick = function(){
+		var parseStr = parseLTSVLog(document.getElementById("log-input").value);
+		createLogTable(document.getElementById("table-container"),parseStr);
+	}
+};
